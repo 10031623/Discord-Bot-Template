3301131fa42803a4f50de27863abc78818fec38d
10031623 <60354589+10031623@users.noreply.github.com>
2020-01-30 13:52:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
-const config = require('./alive.js');
+//const config = require('./alive.js'); //only for people that are using repl.it
 const client = new Discord.Client();
 
-//
-client.login(token);
-//Logs to console that bot is ready and online
-client.once('ready', () => {
-	console.log('Ready!');
-});
 //Stuff that shows up in discord
 client.on('ready', () => {
 	client.user.setPresence({
-			//Shows that the bot is online
-			"status": "online",
 			//Makes it where the bot cannot be AFK
 			"afk": false,
 			//The "game" the bot is playing...
@@ -21,13 +13,25 @@ client.on('ready', () => {
 				"name": "Use " + prefix + "help  for help!"
 			}
 		})
-		.then(console.log("Bot ready."));
-	configSetup();
-})
-
+    console.log('Presence Set!');
+});
+//Sends message in console to show that it is ready
+client.once('ready', () => {
+	console.log('Ready!');
+});
+//COMMANDS
+//DISCORD TEMPLATE
 client.on('message', message => {
 	if (message.content === `${prefix}ping`) {
 		message.channel.send('Pong.');
+	} else if (message.content === `${prefix}beep`) {
+		message.channel.send('Boop.');
+	} else if (message.content === `${prefix}server`) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	} else if (message.content === `${prefix}user-info`) {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
 	}
 });
 
+//Bot Login
+client.login(token);
