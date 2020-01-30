2f286f2f6752190e171acf6ce8c63ff455abc58e
10031623 <60354589+10031623@users.noreply.github.com>
2020-01-30 09:29:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 const config = require('./alive.js');
 const client = new Discord.Client();
 
+//
+client.login(token);
+//Logs to console that bot is ready and online
 client.once('ready', () => {
 	console.log('Ready!');
 });
+//Stuff that shows up in discord
+client.on('ready', () => {
+	client.user.setPresence({
+			//Shows that the bot is online
+			"status": "online",
+			//Makes it where the bot cannot be AFK
+			"afk": false,
+			//The "game" the bot is playing...
+			"game": {
+				"name": "Use " + prefix + "help  for help!"
+			}
+		})
+		.then(console.log("Bot ready."));
+	configSetup();
+}
 
 client.on('message', message => {
-	if (message.content === '!ping') {
+	if (message.content === `${prefix}ping`) {
 		message.channel.send('Pong.');
 	}
 });
 
-client.login(config.token);
