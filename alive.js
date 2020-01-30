66bd8edf69aceadeaf1b40333a817cb4a526e0ad
10031623 <60354589+10031623@users.noreply.github.com>
2020-01-30 09:16:58 -0500

    Create alive.js

---
+++ alive.js
@@ -0,0 +1,9 @@
+//Code to keep the bot running in the background on repl.it
+//Remove "//" before using if you are using on repl.it otherwise delete 
+//"const config = require('./alive.js');" on line "3" in the file "index.js"
+
+//var http = require('http');
+//http.createServer(function (req, res) {
+  //res.write("Online");
+  //res.end();
+//}).listen(8080);
